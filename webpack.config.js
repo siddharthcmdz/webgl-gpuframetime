c76fb2e41881466c233d6a9bf9c7150ac54c1327
Siddharth <siddharth.cmdz@gmail.com>
2021-10-16 19:11:27 -0400

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "index.js",
+        filename: "main[contenthash].js",
         path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: 'index.css', to: '.'}
+            ]
+        })
+    ],
+    module: {
+        rules: [
+            {test: /\.css$/, use: ['style-loader', 'css-loader']}
+        ]
     }
 }
