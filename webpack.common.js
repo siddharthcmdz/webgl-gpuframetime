18493f892c74584267d61177f3ae460144383511
Siddharth <siddharth.cmdz@gmail.com>
2021-10-18 18:32:47 -0400

    split dev and prod webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,10 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
-    output: {
-        filename: "main[contenthash].js",
-        path: path.resolve(__dirname, "dist")
-    },
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/index.html',
